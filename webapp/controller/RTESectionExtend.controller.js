263ef5ca63997c959c0b5e7b2b631a9d9eca5350
pascal-g38 <pascalgolomb@outlook.de>
2024-08-14 10:50:19 +0200

    Mitarbeiterprofil: Loading the RichTextEditor in the Fragment, not through Controller

--- webapp/controller/RTESectionExtend.controller.js
+++ webapp/controller/RTESectionExtend.controller.js
@@ -1,36 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/ControllerExtension",
-    "sap/ui/richtexteditor/RichTextEditor"
-    ], (ControllerExtension, RichTextEditor) => {
+    "sap/ui/core/mvc/ControllerExtension"
+    ], (ControllerExtension) => {
 	"use strict";
     
         return ControllerExtension.extend("employeeprofilemanager.controller.RTESectionExtend", {
         _sectionPrefix: "fe::CustomSubSection::RTESection--",
         override: {   
             onInit() { 
-                this.getView().byId(this._sectionPrefix + "richTextEditorId").attachReady(onRTEready)
- 
-
-
-                // var RichTextEditorObject = new RichTextEditor({
-                //     width: "100%",
-                //     height: "600px",
-                //     showGroupFont: true,
-                //     showGroupLink: true,
-                //     showGroupInsert: true,
-                //     value: sText,
-//                    ready() {
-//                        this.addButtonGroup("styles").addButtonGroup("table")
-//                    }
-                // })
- //               this.getView().byId(this._sectionPrefix + "richTextEditorId").addItem(RichTextEditorObject);
+                 this.getView().byId(this._sectionPrefix + "richTextEditorId").attachReady(function(){
+                    console.log("Hello World")
+                    this.getView().byId(this._sectionPrefix + "richTextEditorId").addButtonGroup("styles").addButtonGroup("table")
+                 }.bind(this)) 
             }
-
-          },
-            onRTEready:function(){
-                console.log("Hello World")
-                this.getView().byId(this._sectionPrefix + "richTextEditorId").addButtonGroup("styles").addButtonGroup("table")
-            }        
+          }      
         });
        });  
     
