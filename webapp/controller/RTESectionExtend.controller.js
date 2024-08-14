ceab1e49442cc74ed616e1a542951bcbcc66a32b
pascal-g38 <pascalgolomb@outlook.de>
2024-08-14 10:30:52 +0200

    Mitarbeiterprofil: Current Version 14.08.2024

--- webapp/controller/RTESectionExtend.controller.js
+++ webapp/controller/RTESectionExtend.controller.js
@@ -8,21 +8,29 @@ sap.ui.define([
         _sectionPrefix: "fe::CustomSubSection::RTESection--",
         override: {   
             onInit() { 
-     			var sText = '<p style="text-align: justify; background: white; font-size: 10pt; font-family: Calibri, sans-serif;"><strong><span style="font-size: 10.5pt; font-family: sans-serif; color: black;">Hier könnte ihr Text stehen</span></strong>';
-    
-                var RichTextEditorObject = new RichTextEditor({
-                    width: "100%",
-                    height: "600px",
-                    showGroupFont: true,
-                    showGroupLink: true,
-                    showGroupInsert: true,
-                    value: sText,
-                    ready() {
-                        this.addButtonGroup("styles").addButtonGroup("table")
-                    }
-                })
-                this.getView().byId(this._sectionPrefix + "richTextEditorId").addItem(RichTextEditorObject);
+                this.getView().byId(this._sectionPrefix + "richTextEditorId").attachReady(onRTEready)
+ 
+
+
+                // var RichTextEditorObject = new RichTextEditor({
+                //     width: "100%",
+                //     height: "600px",
+                //     showGroupFont: true,
+                //     showGroupLink: true,
+                //     showGroupInsert: true,
+                //     value: sText,
+//                    ready() {
+//                        this.addButtonGroup("styles").addButtonGroup("table")
+//                    }
+                // })
+ //               this.getView().byId(this._sectionPrefix + "richTextEditorId").addItem(RichTextEditorObject);
             }
-          }});
+
+          },
+            onRTEready:function(){
+                console.log("Hello World")
+                this.getView().byId(this._sectionPrefix + "richTextEditorId").addButtonGroup("styles").addButtonGroup("table")
+            }        
+        });
        });  
     
